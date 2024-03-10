4fe9d5385c716d9f3c31a3042a74b7bb512a5e8d
DQN08 <129065534+DQN08@users.noreply.github.com>
2024-03-10 10:39:44 +0700

    Add files via upload

--- GRAPH5 - KNIGHT.cpp
+++ GRAPH5 - KNIGHT.cpp
@@ -1,31 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<pair<int, int> >  vec;
-int m, n, k;
+vector<pair<int, int> > vec;
+int dx[] = {-2, -2, -1, -1, 1, 1, 2, 2};
+int dy[] = {-1, 1, -2, 2, -2, 2, -1, 1};
+int dis[1005][1005];
 bool visit[1005][1005];
-void dfs(int x, int y, int k){
-	if(k < 0 || x < 1 || x > n || y < 1 || y > m || visit[x][y])
-		return;
-	visit[x][y] = 1;
-	vec.push_back(make_pair(x, y));
-	dfs(x - 2, y + 1, k - 1);
-	dfs(x - 2, y - 1, k - 1);
-	dfs(x - 1, y + 2, k - 1);
-	dfs(x - 1, y - 2, k - 1);
-	dfs(x + 1, y + 2, k - 1);
-	dfs(x + 1, y - 2, k - 1);
-	dfs(x + 2, y - 1, k - 1);
-	dfs(x + 2, y + 1, k - 1);
-}
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	int x, y;
-	cin >> n >> m >> k >> x >> y;	
+	int x, y, n, m, k;
 
-	dfs(x, y, k);
-	sort(vec.begin(), vec.end());
+	cin >> n >> m >> k >> x >> y;	
+	queue<pair<int, int> > q;
+	q.push(make_pair(x, y));
+	visit[x][y] = 1;
+	dis[x][y]=k;
+	vec.push_back(make_pair(x, y));
+
+	while(!q.empty()){
+		pair<int, int> p = q.front();
+		q.pop();
+		for(int i = 0; i < 8; i++){
+			int x = p.first + dx[i];
+			int y = p.second + dy[i];
+			if(x >= 1 && x <= n && y >= 1 && y <= m && !visit[x][y]&& dis[p.first][p.second]){
+				visit[x][y] = 1;
+				dis[x][y] = dis[p.first][p.second]-1;
+				q.push(make_pair(x, y));
+				vec.push_back(make_pair(x, y));
+			}
+		}
+	}
+	
 	cout << vec.size() << '\n';
+	sort(vec.begin(), vec.end());
 	for(int i = 0; i < vec.size(); i++)
 		cout << vec[i].first << ' ' << vec[i].second << '\n';
 	return 0;
