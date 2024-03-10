b5ef8e3fcc1c8cc840fcf2fccca337bee34fb6f7
DQN08 <129065534+DQN08@users.noreply.github.com>
2024-03-10 14:34:14 +0700

    Đếm số thành phần liên thông

--- BDFS.cpp
+++ BDFS.cpp
@@ -1,14 +1,14 @@
-#includebitsstdc++.h
+#include<bits/stdc++.h>
 using namespace std;
-vectorint a[1000005];
+vector<int> a[1000005];
 bool visit[1000005];
 void bfs(int n){
-	queueint q;
+	queue<int> q;
 	q.push(n);
 	while(!q.empty()){
 		int p = q.front();
 		q.pop();
-		for(int i = 0; i  a[p].size(); i++){
+		for(int i = 0; i < a[p].size(); i++){
 			if(!visit[a[p][i]]){
 				visit[a[p][i]] = 1;
 				q.push(a[p][i]);
@@ -17,24 +17,24 @@ void bfs(int n){
 	}
 }
 int main(){
-	ios_basesync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	int n, m;
-	cin   n  m;
-	for(int i = 1; i = m; i++){
+	cin >> n >> m;
+	for(int i = 1; i <= m; i++){
 		int u, v;
-		cin  u  v;
+		cin >> u >> v;
 		a[u].push_back(v);
 		a[v].push_back(u);
 	}	
 	int ans = 0;
 
-	for(int i = 1; i = n; i++){
+	for(int i = 1; i <= n; i++){
 		if(!visit[i]){
 			ans++;
 			bfs(i);
 		}
 	}
-	cout  ans  'n';
+	cout  << ans << '\n';
 	return 0;
 }
